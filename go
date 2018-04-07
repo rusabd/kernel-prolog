8ae5a7d10b457d5171fc8de961e8cf8474b1c239
Ruslan Abdulkhalikov <rusabd@gmail.com>
2018-04-06 20:53:40 -0700

    Maven based javadoc generation
    Removed unnecessary run/build files

--- go
+++
@@ -1,6 +0,0 @@
-mkdir bin
-pushd .
-cd src
-javac -deprecation -O -d ../bin prolog/*/*.java prolog/*.java
-popd
-./run $1 $2 $3 $4
--- go.bat
+++
@@ -1,6 +0,0 @@
-mkdir bin
-pushd .
-cd src
-javac -deprecation -O -d ../bin prolog/terms/* prolog/fluents/*.java  prolog/io/*.java  prolog/builtins/*.java prolog/*.java
-popd
-run %1 %2 %3 %4
